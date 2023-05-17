777208ee71f16daedf34ff77db0a8b12d8c083f3
AndresAgreda <andrestoleo@outlook.pt>
2023-05-17 13:35:28 +0100

    Arbol de carpetas y maquetacion lista

---
+++ src/app/components/Node.jsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+
+export default function Node({ name, numFiles, numDirs, path }) {
+  const [isSelected, setSelected] = React.useState(true);
+  const fnHandleClick = () => {
+    console.log(path);
+    setSelected(!isSelected);
+  };
+  // const sColor = isSelected ? "#9CD1CE" : "#D9D9D9"; -> Investigar por que no funciona 
+  const sDivClass = isSelected ? "bg-[#9CD1CE] w-6 h-6" : "bg-[#D9D9D9] w-6 h-6";
+  return (
+    <div className="flex flex-col">
+      <div className="flex gap-2 items-center">
+        <div className={sDivClass} onClick={fnHandleClick}></div>
+        <h5 className="text-white"> {name} </h5>
+        <small className="bg-[#D9D9D9] text-black rounded-md px-1 text-xs">
+          Files: {numFiles} Dirs: {numDirs}
+        </small>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/Tree.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Node from "./Node";
+
+export default function Tree({ directories }) {
+  return (
+    <section className="bg-[#3F3838] w-10/12 h-1/2 p-5 flex rounded-sm md:w-1/2 overflow-y-auto">
+      <div className="flex flex-col gap-5">
+        {directories.map((node) => {
+          let aNodesDir = node.nodes.directories;
+          let iNumFiles = node.nodes.numFiles;
+          aNodesDir = aNodesDir.map((nodeS) => {
+            let aNodes = nodeS.nodes.directories;
+            let iNumFiles = nodeS.nodes.numFiles;
+            let sPath = `${node.name}/${nodeS.name}`;
+            return <Node name={nodeS.name} numFiles={iNumFiles} numDirs={aNodes.length} path={sPath} key={sPath}/>;
+          });
+          return (
+            <div className="flex flex-col">
+              <Node name={node.name} numFiles={iNumFiles} numDirs={aNodesDir.length} path={`/${node.name}`} />
+              {aNodesDir.length ? (
+                <div className="flex flex-col gap-3 ml-5 pl-2 mt-5 border-l-[#D9D9D9] border-l-[1px]">
+                  {aNodesDir}
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
